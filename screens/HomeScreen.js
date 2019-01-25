7def8b76784bf4bc15bc610348809f7f35986777
Alvaro Morales <gizmoagm@gmail.com>
2019-01-24 20:15:02 -0800

    Test 3 Checkout

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload</Text>
+              Test 3 Test 3</Text>
           </View>
 
           <View style={styles.helpContainer}>
